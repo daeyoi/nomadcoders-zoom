419b70f81e17f10625259d60603035acbaca9f42
ZizonyeiZ <daeyoi96@gmail.com>
2023-03-14 18:01:59 +0900

    ~#1.2 WebSockets in NodeJS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,8 @@
 //서버가 될 파일 
+
+//http: 기본적으로 node에 설치 되어 있음
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
 // == const express = require('express') 인건가?
 const app = express();
@@ -27,4 +31,20 @@ console.log(" | |_| |/ _ ) | |/ _ )  |  )| |/ _ ) / _ )| '_ ` _ )| | |");
 console.log(" |  _  |  __/ | | (_) | | |)  | (_) | (_) | | | | | |_|_|");
 console.log(" |_| |_|)___|_|_|)___/  |_| )_|)___/ )___/|_| |_| |_(_|_)");
 
-app.listen(3000);
+
+const handlerListen = () => console.log(`Listening on http://localhost:3000`);
+
+
+//createServere를 하려면 requestListner(=app)가 있어야 함
+//http 서버
+const server = http.createServer(app);
+
+//ws 서버
+//같은 서버에서 http, ws서버 두개를 돌릴 수 있음.
+const wss = new WebSocket.Server({ server });
+
+//http서버에 연결
+//http 위에 있는 ws 서버 위에도 연결
+server.listen(3000, handlerListen);
+
+// app.listen(3000, handlerListen);
